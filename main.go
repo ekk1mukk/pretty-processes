b42e42f33de881b52df6060e0ec96dd762a974aa
ekk1mukk <quartzmukk@proton.me>
2024-08-04 19:57:44 +0200

    Added memory bytes, update every 3 seconds, various changes and improvments

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"os"
 	"sort"
+	"time"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
@@ -13,37 +15,33 @@ import (
 	"github.com/shirou/gopsutil/v4/process"
 )
 
-// Define styling for different parts of the UI
-var docStyle = lipgloss.NewStyle().Margin(1, 2) // Add margins to avoid clipping
+var appStyle = lipgloss.NewStyle().Margin(1, 2)
 var itemTitleStyle = lipgloss.NewStyle().PaddingLeft(4).Foreground(lipgloss.Color("#DDDDDD")).Bold(true)
 var itemDescStyle = lipgloss.NewStyle().PaddingLeft(4).Foreground(lipgloss.Color("#3F88F5"))
 var selectedItemTitleStyle = lipgloss.NewStyle().PaddingLeft(4).Foreground(lipgloss.Color("#BAFFDF")).Bold(true)
 var selectedItemDescStyle = lipgloss.NewStyle().PaddingLeft(4).Foreground(lipgloss.Color("#42BFDD")).Bold(true)
 
-// Define an item struct that implements the list.Item interface
 type item struct {
-	pid     int32
-	name    string
-	cmdline string
-	ram     float32
-	cpu     float64
+	pid       int32
+	name      string
+	cmdline   string
+	ram       float32
+	ramAmount uint64
+	cpu       float64
+	ppid      int32
 }
 
-// Implement the list.Item interface
 func (i item) Title() string { return fmt.Sprintf("(%d) %s", i.pid, i.name) }
 
 func (i item) Description() string {
-	// Multi-line description with RAM and CPU usage
-	return fmt.Sprintf("CMD: %s\nRAM: %.2f%% CPU: %.2f%%", i.cmdline, i.ram, i.cpu)
+	return fmt.Sprintf("CMD: %s\nRAM: (%.2f%%) %d CPU: %.2f%% PPID: %d", i.cmdline, i.ram, i.ramAmount, i.cpu, i.ppid)
 }
 
 func (i item) FilterValue() string { return i.name + i.cmdline }
 
-// Custom delegate to handle multi-line items
 type itemDelegate struct{}
 
-// Set the height to match the number of lines in the item's description
-func (d itemDelegate) Height() int { return 3 } // Adjusted to match the number of lines in the item
+func (d itemDelegate) Height() int { return 3 }
 
 func (d itemDelegate) Spacing() int { return 1 }
 
@@ -58,34 +56,36 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, i list.Item) 
 	title := itemTitleStyle.Render(it.Title())
 	desc := itemDescStyle.Render(it.Description())
 
-	// Apply selected item styling if the current item is selected
 	if index == m.Index() {
-		// Use the selectedItemStyle to render the selected item in bold
 		title = selectedItemTitleStyle.Render("> " + it.Title())
 		desc = selectedItemDescStyle.Render(it.Description())
 	}
 
-	// Write the styled output to the io.Writer
-	fmt.Fprintf(w, "%s\n%s", title, desc) // Removed extra newline for concise display
+	fmt.Fprintf(w, "%s\n%s", title, desc)
 }
 
-// Define the model struct for Bubble Tea
+type refreshMsg struct{}
+
 type model struct {
 	list     list.Model
 	quitting bool
 }
 
-// Init initializes the program
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(m.refreshProcesses())
 }
 
-// Update handles incoming messages and updates the model state
+func refreshCmd() tea.Cmd {
+	return func() tea.Msg {
+		time.Sleep(3 * time.Second)
+		return refreshMsg{}
+	}
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		// Adjust the list dimensions based on window size
-		m.list.SetSize(msg.Width, msg.Height-docStyle.GetVerticalFrameSize())
+		m.list.SetSize(msg.Width, msg.Height-appStyle.GetVerticalFrameSize())
 		return m, nil
 
 	case tea.KeyMsg:
@@ -94,6 +94,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.quitting = true
 			return m, tea.Quit
 		}
+
+	case refreshMsg:
+		newItems := getProcesses()
+		m.list.SetItems(newItems)
+		m.list.Title = "pretty-processes v0.0.3 | " + getCurrentTime()
+		return m, refreshCmd()
 	}
 
 	var cmd tea.Cmd
@@ -101,17 +107,20 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-// View renders the current view
 func (m model) View() string {
 	if m.quitting {
 		return "Bye!\n"
 	}
 
-	return docStyle.Render(m.list.View())
+	return appStyle.Render(m.list.View())
 }
 
-// getProcesses retrieves and sorts the processes by PID in descending order
+func getCurrentTime() string {
+	return time.Now().Format("15:04:05")
+}
+
 func getProcesses() []list.Item {
+	ctx := context.Background()
 	processes, err := process.Processes()
 	if err != nil {
 		log.Fatalf("Error fetching processes: %s", err)
@@ -121,25 +130,58 @@ func getProcesses() []list.Item {
 
 	for _, proc := range processes {
 		name, err := proc.Name()
-		cmdline, err := proc.Cmdline()
-		ramPercentage, err := proc.MemoryPercent()
-		cpuPercentage, err := proc.CPUPercent()
-
 		if err != nil {
-			log.Printf("Error fetching process info: %s", err)
+			log.Printf("Error fetching process name: %s", err)
 			continue
 		}
-		processList = append(processList, item{pid: proc.Pid, name: name, cmdline: cmdline, ram: ramPercentage, cpu: cpuPercentage})
+
+		cmdline, err := proc.CmdlineWithContext(ctx)
+		if err != nil {
+			log.Printf("Error fetching cmdline: %s", err)
+			continue
+		}
+
+		ramPercentage, err := proc.MemoryPercentWithContext(ctx)
+		if err != nil {
+			log.Printf("Error fetching RAM usage: %s", err)
+			continue
+		}
+
+		ramAmount, err := proc.MemoryInfoExWithContext(ctx)
+		if err != nil {
+			log.Printf("Error fetching CPU usage: %s", err)
+			continue
+		}
+
+		cpuPercentage, err := proc.CPUPercentWithContext(ctx)
+		if err != nil {
+			log.Printf("Error fetching CPU usage: %s", err)
+			continue
+		}
+
+		ppid, err := proc.Ppid()
+		if err != nil {
+			log.Printf("Error fetching PPID: %s", err)
+			continue
+		}
+
+		processList = append(processList, item{
+			pid:       proc.Pid,
+			name:      name,
+			cmdline:   cmdline,
+			ram:       ramPercentage,
+			ramAmount: ramAmount.RSS,
+			cpu:       cpuPercentage,
+			ppid:      ppid,
+		})
 	}
 
-	// Sort the processList by PID in descending order
 	sort.Slice(processList, func(i, j int) bool {
 		return processList[i].pid > processList[j].pid
 	})
 
 	var processItems []list.Item
 
-	// Convert sorted processList to list.Items
 	for _, proc := range processList {
 		processItems = append(processItems, proc)
 	}
@@ -147,15 +189,20 @@ func getProcesses() []list.Item {
 	return processItems
 }
 
+func (m *model) refreshProcesses() tea.Cmd {
+	return func() tea.Msg {
+		return refreshMsg{}
+	}
+}
+
 func main() {
 	items := getProcesses()
 
-	// Initialize the Bubble Tea list with a delegate
 	delegate := itemDelegate{}
-	l := list.New(items, delegate, 0, 0) // Initialize with zero to auto-resize later
-	l.Title = "pretty-processes v0.0.2"
-	l.SetShowStatusBar(true)    // Enable status bar for better UX
-	l.SetFilteringEnabled(true) // Enable filtering
+	l := list.New(items, delegate, 0, 0)
+	l.Title = "pretty-processes v0.0.3 | " + getCurrentTime()
+	l.SetShowStatusBar(true)
+	l.SetFilteringEnabled(true)
 	l.SetShowTitle(true)
 
 	m := model{list: l}
